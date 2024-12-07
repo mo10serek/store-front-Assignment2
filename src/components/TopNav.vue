6f495da66e5c817b7a07f727526ab5152df8c22e
MichaelBalcerzak <balcerzak.michael@gmail.com>
2024-12-07 13:52:17 -0500

    change color in main

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -40,7 +40,7 @@ nav {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: #2907e9d7;
+  background-color: #f50cfdd7;
   color: #fff;
   padding-top: 0.5rem;
   padding-left: 1rem;
