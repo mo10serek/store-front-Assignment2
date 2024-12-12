4a516c3a33e08cc618844608b06c719c53607300
MichaelBalcerzak <balcerzak.michael@gmail.com>
2024-12-12 15:17:41 -0500

    change colour

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -40,7 +40,7 @@ nav {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: #fc07d3d7;
+  background-color: #d82b82d7;
   color: #fff;
   padding-top: 0.5rem;
   padding-left: 1rem;
